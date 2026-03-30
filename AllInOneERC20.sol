7329df0917e8cd63d69347912882e8c58e8b7432
jeack <web028web028@gmail.com>
2026-03-31 01:40:27 +0800

    Create AllInOneERC20.sol

---
+++ AllInOneERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract AllInOneERC20 is ERC20,Ownable {
+    constructor() ERC20("AllInOne","AIO"){_mint(msg.sender,1000000 ether);}
+    function mint(address t,uint256 a) public onlyOwner {_mint(t,a);}
+    function burn(uint256 a) public {_burn(msg.sender,a);}
+}
