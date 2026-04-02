4fa739faadaeb7a8d70b0507dcac4bc3bf0a463f
jeack <web028web028@gmail.com>
2026-04-02 11:54:35 +0800

    Update AllInOneERC20.sol

--- AllInOneERC20.sol
+++ AllInOneERC20.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract AllInOneERC20 is ERC20,Ownable {
     constructor() ERC20("AllInOne","AIO"){_mint(msg.sender,1000000 ether);}
     function mint(address t,uint256 a) public onlyOwner {_mint(t,a);}
